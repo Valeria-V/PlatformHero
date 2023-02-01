56d2687a946aefc7ddd9be6e1419485633d6a504
Valéria de Oliveira <valv1993@hotmail.com>
2023-02-01 09:54:00 -0300

    first commit

---
+++ Assets/GameFolder/Player/Script/PlayerController.cs
@@ -0,0 +1,47 @@
+// **** BIBLIOTECAS **** //
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Classe necessaria MonoBehavior
+public class PlayerController : MonoBehaviour
+{
+    // **** VARIAVEIS **** //
+    Rigidbody2D rb;
+
+    [SerializeField] Vector2 force;
+
+    public Transform floorCollider;
+    [SerializeField] Vector2 vel;
+
+
+    // Funcao Start - inicio *"Sempre que houver valores de fisica, criar variaveis no Start"
+    void Start()
+    {  
+        rb      = GetComponent<Rigidbody2D>();
+        force   = new Vector2(0, 120);
+    }
+
+    // Funcao Loop - sempre rodando e atualizando 
+    void Update()
+    {
+        // **** MOVIMENTO **** //
+        // Jump
+        if (Input.GetButtonDown("Jump") && floorCollider.GetComponent<FloorCollider>().canJump == true)
+        {
+            rb.velocity = Vector2.zero;
+            floorCollider.GetComponent<FloorCollider>().canJump = false;
+            rb.AddForce(force);
+        }
+        vel = new Vector2(Input.GetAxisRaw("Horizontal"), rb.velocity.y);   // Horizontal x1, y-1
+
+    }
+
+    // Funcao FixedUpdate - Nivela FPS para todos sempre atualizara a cada 0.2s
+    private void FixedUpdate()
+    {
+        // Velocidade player nivelada
+        rb.velocity = vel;
+    }
+}
