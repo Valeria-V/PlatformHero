a457e954fddfb180c2e7957d208c438699ae9413
Valéria de Oliveira <valv1993@hotmail.com>
2023-02-02 11:24:14 -0300

    att

--- Assets/GameFolder/Player/Script/FloorCollider.cs
+++ Assets/GameFolder/Player/Script/FloorCollider.cs
@@ -5,22 +5,22 @@ using UnityEngine;
 public class FloorCollider : MonoBehaviour
 {
 
-    // **** VARIAVEIS ****//
+    // **** VARIABLES ****//
     public bool canJump;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    // Funcao de colisao
+    // FunctionCollision
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Floor"))
